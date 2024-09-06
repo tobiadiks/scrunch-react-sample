35387cb105d5d4a7837b17b7d62be3472d8ff046
tobiadiks <tobiadiks@gmail.com>
2024-09-06 02:56:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import compressImageClient  from 'scrunchjs';
 
-function App() {
+const ImageUploader = () => {
+  const [image, setImage] = useState(null);
+  const [compressedImage, setCompressedImage] = useState(null);
+  const [maxWidth, setMaxWidth] = useState(1920);
+  const [initialQuality, setInitialQuality] = useState(70);
+  const [minQuality, setMinQuality] = useState(10);
+  const [maxFileSize, setMaxFileSize] = useState(200);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
+
+  const handleUpload = async () => {
+   
+    if (!image) return;
+
+    const compressedBuffer = await compressImageClient({
+      input: image,
+      maxWidth,
+      initialQuality,
+      minQuality,
+      maxFileSize: maxFileSize * 1024
+    });
+
+    const compressedImage = new Image();
+    compressedImage.src = URL.createObjectURL(compressedBuffer);
+    setCompressedImage(compressedImage.src);
+    console.log(compressedImage.src)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" onChange={handleImageChange} />
+      <div>
+        <label>Max Width: <input type="number" value={maxWidth} onChange={(e) => setMaxWidth(Number(e.target.value))} /></label>
+        <label>Initial Quality: <input type="number" value={initialQuality} onChange={(e) => setInitialQuality(Number(e.target.value))} /></label>
+        <label>Min Quality: <input type="number" value={minQuality} onChange={(e) => setMinQuality(Number(e.target.value))} /></label>
+        <label>Max File Size (KB): <input type="number" value={maxFileSize} onChange={(e) => setMaxFileSize(Number(e.target.value))} /></label>
+      </div>
+      <button onClick={handleUpload}>Upload and Compress</button>
+      {compressedImage && <img src={compressedImage} alt="Compressed" />}
     </div>
   );
-}
+};
 
-export default App;
+export default ImageUploader;
